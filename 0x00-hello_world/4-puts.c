39d0b6dd1a0e1a5d13aaf96b007d9c6b2a35da5c
yirga1 <39591778+yirga1@users.noreply.github.com>
2022-06-02 18:04:29 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
  * main - Prints out a string to stdout.
- * Return: 0 if success.
+ * Return: 0 if success
  */
 int main(void)
 {
-	puts("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);
+	puts("Programming is like building a multilingual puzzle,\n");
+	return (0);
 }
-
