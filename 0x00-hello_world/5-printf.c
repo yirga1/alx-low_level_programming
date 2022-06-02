fda1c99402a7374d5552656f6b8f9b707127c83e
yirgalem <yirgapply@gmail.com>
2022-06-02 07:49:57 -0700

    yes

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Prints out a string to stdout.
+ * Returnt: 0 if success
+ * */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return 0;
+}
+
