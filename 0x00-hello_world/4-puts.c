4879d2c0bfc88baa4c5d51666a213bbfbe38f241
yirgalem <yirgapply@gmail.com>
2022-06-02 07:40:08 -0700

    yes

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
+int main()
+{
+	puts("with proper grammar, but the outcome is a piece of art,");
+        return 0;
+}
+
