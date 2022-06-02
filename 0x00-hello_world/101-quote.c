e54aae7bae8133579376337ef9a0b58d6cb72f34
yirga1 <39591778+yirga1@users.noreply.github.com>
2022-06-02 18:58:22 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
  * main - Prints out a string to stdout.
  * Return: 0 if success
  */
 int main(void)
 {
-system("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19" 14);
 	return (0);
 	
 }
