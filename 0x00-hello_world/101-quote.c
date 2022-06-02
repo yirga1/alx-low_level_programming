711854a7cc25a33c9f8e370e95a5a0bda015ea4e
yirga1 <39591778+yirga1@users.noreply.github.com>
2022-06-02 18:55:33 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Prints out a string to stdout.
  * Return: 0 if success
  */
 int main(void)
 {
-write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+system("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (0);
 	
 }
