7d46c424406c85039c2b364dd6faf0872b7fbf02
yirga1 <39591778+yirga1@users.noreply.github.com>
2022-06-02 17:42:05 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main - Prints out a string to stdout.
  * Return: 0 if success.
  */
-int main()
+int main(void)
 {
 	puts("with proper grammar, but the outcome is a piece of art,");
         return 0;
