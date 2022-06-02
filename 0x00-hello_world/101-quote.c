991cebb502d7dfa39af3d23d9d5c4e6b6ab9105e
yirgalem <yirgapply@gmail.com>
2022-06-02 08:37:20 -0700

    yes

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Prints out a string to stdout.
+ * Return: 0 if success
+ */
+int main()
+{
+return("and that piece of art is useful\" \- Dora Korpar, 2015\-10\-19 \n");
+	
+}
+
