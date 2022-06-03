854c4d5b191404964f930033efb4cce064c7cd05
yirga1 <39591778+yirga1@users.noreply.github.com>
2022-06-03 09:48:57 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
-#include<stdio.h>
-#include<unistd.h>
+#include <unistd.h>
 /**
- * main - Entry point
- * Return: 1
+ * main - Prints out a string to stdout.
+ * Return: 1 Always success
  */
 int main(void)
 {
-        write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-        return (1);
-}
+char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, str1, 59);
+	return (1);	
