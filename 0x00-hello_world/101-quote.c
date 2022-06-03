1d30a7dff3bc709512b61520a2df2f14a475fde3
yirga1 <39591778+yirga1@users.noreply.github.com>
2022-06-03 09:46:38 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
-#include <stdio.h>     
+#include<stdio.h>
+#include<unistd.h>
 /**
- * main - Prints out a string to stdout.
- * Return: 1 Always success
+ * main - Entry point
+ * Return: 1
  */
-int main(void)                                                                                 
-{                                                                                              
-        fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, ");                  
-        fprintf(stderr, "2015-10-19\n");                                                       
-        return (1);                                                                            
+int main(void)
+{
+        write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+        return (1);
 }
