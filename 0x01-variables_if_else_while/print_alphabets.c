33977a9ad64f314e96dba616cca8c72e3b06b49b
yirgalem <yirgapply@gmail.com>
2022-06-04 12:32:17 -0700

    yes

---
+++ 0x01-variables_if_else_while/print_alphabets.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * maiin - prints lowercase the uppercase.
+ * Return: 0 if success
+ */
+int main(void)
+{
+char a = 'a';
+char b = 'A';
+while (a <= 'z')
+putchar(a);
+a++;
+}
+while (b <= 'Z')
+{
+putchar(d);
+d++;
+}
+putchar('/n');
+return (0);
+}
