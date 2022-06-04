d0518f51eeeee270182fdadb65280a1536aa46e6
yirga1 <39591778+yirga1@users.noreply.github.com>
2022-06-04 12:53:38 -0700

    Create 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - reverse alphabet
+ * Return: 0 if success
+ */
+int main(void)
+{
+char c = 'z';
+while (c >= 'a')
+{
+putchar(c);
+c--;
+}
+putchar('\n');
+return (0);
+}
