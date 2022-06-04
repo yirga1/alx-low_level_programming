2357e8f452eb001c38c70e236ab3f1db2a13d895
yirga1 <39591778+yirga1@users.noreply.github.com>
2022-06-04 12:36:35 -0700

    Create 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Prints lowercase then  uppercase.
+ * Return: 0 if successful
+ */
+int main(void)
+{
+char a = 'a';
+charb = 'A';
+while (a <= 'z')
+{
+putchar(a);
+a++;
+}
+while (b <= 'Z')
+{
+putchar(d);
+b++;
+}
+putchar('\n');
+return (0);
+}
