d3c9704516a0324bdec9e8334a55925c01433789
yirgalem <yirgapply@gmail.com>
2022-06-03 23:50:31 -0700

    yes

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase.
+ * Return: Always 0.
+ */
+int main(void)
+{
+ char l;
+ for (l = 'a'; l <= 'z'; l++)
+ putchar(letter);
+ putchar('\n');
+ return (0);
+}
