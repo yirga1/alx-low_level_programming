7dde1cfd2da97412ee99b521ec42c3b30ed75200
yirgalem <yirgapply@gmail.com>
2022-06-04 08:16:02 -0700

    yes

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - all combinations of single digits
+ * Return: 0 for success
+ */
+int j = '0';
+while (j <= '9')
+{
+putchar(j);
+if (j != '9')
+{
+putchar(',');
+putchar(' ');
+}
+j++;
+}
+putchar('\n');
+return (0);
+}
