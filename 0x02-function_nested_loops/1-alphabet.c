af30fcd4775223489ea755af91629856c30c2ca5
yirgalem <yirgapply@gmail.com>
2022-06-07 11:33:55 -0700

    yes

---
+++ 0x02-function_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - print alphabet
+ * Return: 0 success
+ */
+void print_alphabet(void)
+{
+char ch;
+for(ch = 'a'; c <= 'z'z; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+return (0);
+}
