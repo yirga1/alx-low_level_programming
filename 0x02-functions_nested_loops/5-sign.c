f2cd43e05cf988b5b95cf5bd817f29e54dbb2a8c
yirgalem <yirgapply@gmail.com>
2022-06-07 12:43:59 -0700

    yes

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - prints the sign of a number.
+ * Return: Always 0.
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar(43);
+return (1);
+}
+else if (n == 0)
+{
+_putchar(48);
+return (0);
+}
+else
+{
+_putchar(45);
+return (-1);
+}
+}
