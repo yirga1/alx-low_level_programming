0f3042530071a598d2104d40b27094a2a6b3e412
yirgalem <yirgapply@gmail.com>
2022-06-08 06:05:45 -0700

    yes

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - adds two integers and return result.
+ *
+ * Return: Always 0.
+ */
+int
+add(int a, int b) {
+return a + b;
+}
+int main(void)
+{
+int n;
+n = add(89, 9);
+printf("%d\n", n);
+return (0);
+}
+
