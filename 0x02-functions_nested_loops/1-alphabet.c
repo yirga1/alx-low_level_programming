aaa0d3ee9bab9162ea59ba517c52f36f513d9ea1
yirgalem <yirgapply@gmail.com>
2022-06-08 01:15:01 -0700

    yes

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,13 @@
-#include "main.h"
+include "main.h"
 /**
  * main - print alphabet
  * Return: 0 success
  */
 void print_alphabet(void)
 {
+/**
+ * Discription - about printing alphabet
+ */
 char ch;
 for (ch = 'a'; ch <= 'z'; ch++)
 {
