c645b99000b02f91804b04b5ccc75ccf214982b6
yirgalem <yirgapply@gmail.com>
2022-06-09 06:33:56 -0700

    yes

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int isupper(int c);
+char c;
+c = 'A';
+printf("%c: %d\n", c, isupper(c));
+c = 'a';
+printf("%c: %d\n", c, isupper(c));
+return (0);
+}
