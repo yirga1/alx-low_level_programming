aeb4256ef62327cb82df250d549702b3d83ddf63
yirga1 <39591778+yirga1@users.noreply.github.com>
2022-06-10 16:03:30 +0300

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,14 @@
 #include "main.h"
-
 /**
  * _isupper - check for uppercase
- *
  * Description: Prints t with _putchar
  * @c: charater
  * Return: void
  */
-
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-		return (1);
-	else
-		return (0);
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
 }
