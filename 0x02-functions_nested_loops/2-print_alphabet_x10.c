8c0c62afe0193f7a1c93dc4bcfe342fd151c50c5
yirga1 <39591778+yirga1@users.noreply.github.com>
2022-06-10 17:20:44 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,15 +7,15 @@
 
 void print_alphabet_x10(void)
 {
-  char c;
-  int i;
+char c;
+int i;
 
-  for (i = 0; i < 10; i++)
-  {
-    for (c = 'a'; c <= 'z'; c++)
-    {
-      _putchar(c);
-    }
-    _putchar('\n');
-  }
+for (i = 0; i < 10; i++)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
 }
