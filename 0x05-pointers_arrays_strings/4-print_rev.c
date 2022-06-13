88e304ec17e16c3e3a91d9d5df74f9d2c73a708c
yirga1 <39591778+yirga1@users.noreply.github.com>
2022-06-13 12:38:07 -0700

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,16 +7,15 @@
  */
 void print_rev(char *s)
 {
-	int i = 0;
-
-	while (s[i] != 0)
-	{
-		i++;
-	}
-	while (i > 0)
-	{
-		i--;
-		_putchar(s[i]);
-	}
-	_putchar('\n');
+int i = 0;
+while (s[i] != 0)
+{
+i++;
+}
+while (i > 0)
+{
+i--;
+_putchar(s[i]);
+}
+_putchar('\n');
 }
