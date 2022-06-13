df8b5000dba891f239635405e4af5224631d060d
yirga1 <39591778+yirga1@users.noreply.github.com>
2022-06-13 12:14:27 -0700

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,16 +1,13 @@
 #include "main.h"
-
 /**
  * swap_int - swaps a and b
  * @a: a value
  * @b: b value
- *
  * Return: void
  */
 void swap_int(int *a, int *b)
 {
 	int e;
-
 	e = *a;
 	*a = *b;
 	*b = e;
