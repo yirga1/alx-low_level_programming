a7894f516ce5707d0d1878bf5dd739f61daf4da9
yirga1 <39591778+yirga1@users.noreply.github.com>
2022-06-16 13:27:30 +0300

    Create 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+* _strcmp -> for comparing purpose
+* @s1: parameter1
+* @s2: parameter2
+* Return: integer
+*/
+int _strcmp(char *s1, char *s2)
+{
+  int r;
+  r = strcmp(s1, s2);
+  return (r);
+  }
