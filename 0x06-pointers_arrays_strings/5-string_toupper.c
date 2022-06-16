ba5b0a199f28ff7f5a938ff9ab639fc440048803
yirga1 <39591778+yirga1@users.noreply.github.com>
2022-06-16 14:37:20 +0300

    Create 5-string_toupper.c
    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* string_to_upper -> convert string to upper
+* @x: string parameter
+* Return: string
+*/
+char *string_toupper(char *x)
+{
+  int i = 0;
+  while (x[i])
+  {
+    if (x[i] >= 97 && x[i] <= 122)
+      x[i] = x[i] - 32;
+    i++;
+  }
+return (x);
+}
