a96084346131ee59f52c75ea8113e5602aa46fc3
yirga1 <39591778+yirga1@users.noreply.github.com>
2022-06-16 16:05:24 +0300

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,15 +8,15 @@
 char *leet(char *x)
 {
 int a = 0, b, l = 5;
-char t[5] = {'A', 'E', 'O', 'T', 'L'};
-char tw[5] = {'4', '3', '0', '7', '1'};
+char tr[5] = {'A', 'E', 'O', 'T', 'L'};
+char trw[5] = {'4', '3', '0', '7', '1'};
 while (x[a])
 {
 b = 0;
 while (b < l)
 {
-if (x[a] == t[b] || x[a] - 32 t[b])
-x[a] = tw[b];
+if (x[a] == tr[b] || x[a] - 32 tr[b])
+x[a] = trw[b];
 b++;
 }
 a++;
