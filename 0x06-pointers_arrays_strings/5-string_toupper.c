caab2ce4ce99c79608b096df5283d6b78e9df38b
yirga1 <39591778+yirga1@users.noreply.github.com>
2022-06-16 14:40:00 +0300

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,12 +7,12 @@
 */
 char *string_toupper(char *x)
 {
-  int i = 0;
-  while (x[i])
-  {
-    if (x[i] >= 97 && x[i] <= 122)
-      x[i] = x[i] - 32;
-    i++;
-  }
+int i = 0;
+while (x[i])
+{
+if (x[i] >= 97 && x[i] <= 122)
+x[i] = x[i] - 32;
+i++;
+}
 return (x);
 }
