bdcc661c2943afe5eb63f35ca9c83d59812b4e69
yirga1 <39591778+yirga1@users.noreply.github.com>
2022-06-16 11:52:44 +0300

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <string.h>
-
-/* _strncpy -> copy purpose
+/*
+* _strncpy -> copy purpose
 * @dest: parameter1
 * @src: parameter2
 * n: parameter3
@@ -9,6 +9,6 @@
 */
 char *_strncpy(char *dest, char *src, int n)
 {
-  _strncpy(dest, src, n);
-  return (dest);
+_strncpy(dest, src, n);
+return (dest);
 }
