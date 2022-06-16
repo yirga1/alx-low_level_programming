bca88159e0d82f6764d4c5a943978202c888f20b
yirga1 <39591778+yirga1@users.noreply.github.com>
2022-06-16 13:33:35 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,10 +12,10 @@ int len = 0, i;
 while (dest[len])
 len++;
 for (i = 0; src[i] != 0; i++)
-	{
-		dest[len] = src[i];
-		len += 1;
-	}
+{
+	dest[len] = src[i];
+	len += 1;
+}
 dest[len] = '\0';
 return (dest);
 }
