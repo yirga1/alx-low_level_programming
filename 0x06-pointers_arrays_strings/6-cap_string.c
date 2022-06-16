fee437711731e0062497982a22e51ac705786981
yirga1 <39591778+yirga1@users.noreply.github.com>
2022-06-16 15:16:03 +0300

    Update 6-cap_string.c
    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,19 +7,20 @@
 */
 char *cap_string(char *x)
 {
-  char sp[] = {39, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
-  int len = 13;
-  int a = 0;, i;
-  while (x[a])
-  {
-    i = 0;
-    while (i < len)
-    {
-      if ((a == 0 || s[a - 1] == sp[i]) && (s[a] >= 97 && s[a] <=122))
-        s[a] = s[a] - 32;
-      i++;
-    }
-    a++;
-  }
-  return (x);
+char sp[] = {39, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+int len = 13;
+int a = 0;
+int i;
+while (x[a])
+{
+i = 0;
+while (i < len)
+{
+if ((a == 0 || s[a - 1] == sp[i]) && (s[a] >= 97 && s[a] <=122))
+s[a] = s[a] - 32;
+i++;
+}
+a++;
+}
+return (x);
 }
