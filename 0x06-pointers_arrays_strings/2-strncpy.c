f090b29682c5e8c40307601e3badd9b6a21970d9
yirga1 <39591778+yirga1@users.noreply.github.com>
2022-06-16 11:49:53 +0300

    Create 2-strncpy.c
    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/* _strncpy -> copy purpose
+* @dest: parameter1
+* @src: parameter2
+* n: parameter3
+* Return: string
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+  _strncpy(dest, src, n);
+  return (dest);
+}
