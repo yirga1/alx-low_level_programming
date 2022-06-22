2cdcac52b492bd5c106076427284078b19ba9e97
yirgalem <yirgapply@gmail.com>
2022-06-22 05:36:25 -0700

    hot fix

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,8 +2,8 @@
 
 /**
  * _strlen_recursion - returns the length of a string
-  * @s: the string to check the length of
-  * Return: an integer that displays the length of the string
+ * @s: the string to check the length of
+ * Return: an integer that displays the length of the string
  */
 
 int _strlen_recursion(char *s)
