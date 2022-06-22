9fa4c37b64a69802a3299081f6b158351c18578e
yirgalem <yirgapply@gmail.com>
2022-06-22 05:50:10 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,13 +9,25 @@
 
 int helper(int a, int b)
 {
-        if (b * b > a)
-                return (-1);
-
-        else if (b * b == a)
-                return (b);
-
-        else
-                return (helper(a, b + 1));
-
+	if (b * b > a)
+		return (-1);
+	else if (b * b == a)
+		return (b);
+	else
+		return (helper(a, b + 1));
+	return (1);
 }
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: the number to return the square root of
+ * Return: the square root of @n
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+	return (helper(n, 1));
+}
+
