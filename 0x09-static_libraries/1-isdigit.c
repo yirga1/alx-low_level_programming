8ffdfc6e8fb5eb8cef7aa50e6bb670e5734f8a36
yirga1 <39591778+yirga1@users.noreply.github.com>
2022-06-25 22:56:09 +0300

    Update 1-isdigit.c

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,12 +1,16 @@
-include "main.h"
+#include "main.h"
 
 /**
- * _isdigit - check the code for alx School students.
- * @c: is an integer parameter
+ * _isdigit - check the code for Holberton School students.
+ *
+ * @c: is an integer param
+ *
  * Return: Always 0.
  */
 
+
 int _isdigit(int c)
 {
+
 	return (c >= 48 && c <= 57);
 }
