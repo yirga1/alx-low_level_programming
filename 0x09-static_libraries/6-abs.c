cda226705cf320988e2f2beeacb01297b8d43e4d
yirga1 <39591778+yirga1@users.noreply.github.com>
2022-06-25 22:23:10 +0300

    Create 6-abs.c

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - check the code for alx students.
+ * @n: is an integer argument
+ * Return: Always 0.
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
