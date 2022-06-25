4a6fa71d83be249a9939c9f10939491a727474db
yirga1 <39591778+yirga1@users.noreply.github.com>
2022-06-25 21:41:03 +0300

    Create 0-isupper.c

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - check the code for alx students.
+ * @c: is an integer param
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90);
+}
