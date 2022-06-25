884f4ef4bf8356506d56c9123bf5808fa61d1e7c
yirga1 <39591778+yirga1@users.noreply.github.com>
2022-06-25 21:48:19 +0300

    Create 1-isdigit.c

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,12 @@
+include "main.h"
+
+/**
+ * _isdigit - check the code for alx School students.
+ * @c: is an integer parameter
+ * Return: Always 0.
+ */
+
+int _isdigit(int c)
+{
+	return (c >= 48 && c <= 57);
+}
